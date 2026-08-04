1ebe70cc10397d5399ea77912a3bb7e566ba24da
Matthew Miller <mwm@fastmail.com>
2026-08-04 17:42:04 +0200

    Repro: System.get_env/1 in component helper compiles but fails in client runtime (:os.getenv/1 not ported)

--- app/home_page.ex
+++ app/home_page.ex
@@ -1,11 +1,15 @@
 defmodule HologramSkeleton.HomePage do
   use Hologram.Page
-  
+
   route "/"
-  
+
   layout HologramSkeleton.DefaultLayout
-  
+
+  defp host do
+    System.get_env("PHX_HOST") || "example.com"
+  end
+
   def template do
-    ~HOLO"<h1>Hello from Hologram!</h1>"
+    ~HOLO"<h1>Hello from {host()}!</h1>"
   end
 end
